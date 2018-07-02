4c1686fd9b6f42cf6d4485ff629c15de3a83bdd6
François MATHIEU <francois.mathieu@livexp.fr>
2018-07-02 13:19:56 +0200

    Add IndexServlet and JEE libs

---
+++ src/servlet/IndexServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
+public class IndexServlet extends javax.servlet.http.HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
